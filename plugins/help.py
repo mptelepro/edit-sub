d25222f281084d0e49811130949169edecabc240
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 15:55:22 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -39,20 +39,20 @@ def help_answer(client, callback_query):
 def map(pos):
     if(pos==1):
         button = [
-            [InlineKeyboardButton(text = '▶️', callback_data = "help+2")]
+            [InlineKeyboardButton(text = '▶️Go', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://github.com/DamienSoukara/FSub-Heroku"
         button = [
             [InlineKeyboardButton(text = '🗣 Support Chat', url="https://t.me/damienhelp")],
             [InlineKeyboardButton(text = '🤖 Source Code', url=url)],
-            [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
+            [InlineKeyboardButton(text = '◀️Back', callback_data = f"help+{pos-1}")]
         ]
     else:
         button = [
             [
-                InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}"),
-                InlineKeyboardButton(text = '▶️', callback_data = f"help+{pos+1}")
+                InlineKeyboardButton(text = '◀️Back', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '▶️Go', callback_data = f"help+{pos+1}")
             ],
         ]
     return button
