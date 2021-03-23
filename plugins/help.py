be71c72730e8c06126e4b5bee11d4a47caadb1aa
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 21:18:18 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -56,8 +56,8 @@ def map(pos):
         button = [
             [InlineKeyboardButton(text = '😈SUPPORT CHAT😈', url="https://t.me/mpazaabot")],
             [InlineKeyboardButton(text = '🤖 Source Code', url=url)],
-            [InlineKeyboardButton(text = 'HELP', callback_data = f"help+{pos-1}")]
-            InlineKeyboardButton(text = 'NOT', callback_data = f"help+{pos+1}")
+            [InlineKeyboardButton(text = 'HELP', callback_data = f"help+{pos-1}")],
+            [InlineKeyboardButton(text = 'NOT', callback_data = f"help+{pos+1}")]
         ]
     else:
         button = [
