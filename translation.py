868d6e704b6661493a90b431cd681b371d49177a
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 23:10:30 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,16 @@
+class Translation(object):
+    START_TEXT = """
+Hello {}, I' am small media or file to telegra.ph link uploader bot.
+
+- Just give me a media under 5MB
+- Then I will download it
+- I will then upload it to the telegra.ph link
+
+Made by @cybersecurityhacker
+"""
+    DOWNLOAD_TEXT = "<code>Downloading to My Server ...</code>"
+    UPLOADING_TEXT = "<code>Downloading Completed. Now I'am Uploading to telegra.ph Link ...</code>"
+    SOMETHING_WRONG = "Something Wrong! Contact @cybersecurityhacker."
+    FREE_USER_LIMIT_Q_SZE = "Sorry Friend, Free users can only 1 request per {} minutes. Please try again after {} seconds later."
+    FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
+    BANNED_USER_TEXT = "<code>You are Banned!</code>"
