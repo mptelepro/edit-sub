bdcb6eb075481d37be27209beadc7f3f458d0e5f
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 22:23:47 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -54,7 +54,7 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-2):
         url = "https://t.me/mpazaanbot"
         button = [
-            [InlineKeyboardButton(text = '😈SUPPORT CHAT😈', url="https://t.me/mpazaabot")], [InlineKeyboardButton(text = '😈SUPPORT CHAT😈', url="https://t.me/mpazaabot")],
+            [InlineKeyboardButton(text = '😈SUPPORT CHAT😈', url="=url")], [InlineKeyboardButton(text = '😈SUPPORT CHAT😈', url="https://t.me/mpazaabot")],
             [InlineKeyboardButton(text = '🤖SOURCE CODE🤖', url=url)],
             [InlineKeyboardButton(text = '😆HELP😆', callback_data = f"help+{pos-1}")],
             [InlineKeyboardButton(text = '😂NO OPEN😀', callback_data = f"start+{pos-1}")]
