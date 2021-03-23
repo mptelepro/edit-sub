69452f1e3dd9011e43b8a8c3e16cbbf816904e1a
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 23:00:57 +0530

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,6 @@
+from pyrogram.types import InlineKeyboardButton
+
+class Button(object):
+
+    START_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/cybersecurityhacker'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/cybersecurityhacker')]]
+                                      
