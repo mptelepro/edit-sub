13bf98e208637ae7124a46f1b7c1504198b94ab5
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 20:32:03 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -48,7 +48,7 @@ def map(pos):
     if(pos==1):
         button = [
             [InlineKeyboardButton(text = '🗣 Support Chat', url="https://t.me/damienhelp")]
-            [InlineKeyboardButton(text = 'text = '▶️', callback_data = "help+2")]
+            [InlineKeyboardButton(text = 'text = '▶️', callback_data = "help")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://github.com/DamienSoukara/FSub-Heroku"
