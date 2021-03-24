2c4ac4968ac4ed7dd60bba9963e0cab45da8c5d8
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-24 11:42:13 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -2,8 +2,8 @@ import logging
 from config import Messages as tr
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from translation import Translation
-from buttons import Button
+
+
 
 logging.basicConfig(level=logging.INFO)
 
